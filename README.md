08e4cf542f304b7950f184c36554dd53e6ce67b7
wolxXx <devops@wolxXx.de>
2025-03-05 09:09:44 +0100

    Add Xdebug-switcher script and project setup files
    Introduce `xdebug.js` for toggling Xdebug sessions without browser extensions. Update documentation and include `bower.json`, `package.json`, and `.gitignore` for project configuration. Currently supports PHPStorm, with plans for more IDEs.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # xdebug-switcher
 simple js class for enabling or disabling xdebug cookie
+
+as the chrome extension is dead and it is not very convenient to install extensions, add this xdebug.js snippet to your website and enable or disable xdebug sessions right in the page. 
+
+you may add css styles for .xdebug-helper
+
+currently only works for phpstorm, as it is needed right now. 
+other variations are planned. 
---
+++ xdebug.js
@@ -0,0 +1,63 @@
+class XdebugHelper {
+    constructor() {
+        this.enabled = false;
+        this.cookieName = 'XDEBUG_SESSION';
+        this.variations = {
+            'XDEBUG_ECLIPSE': 'eclipse',
+            'netbeans-xdebug': 'netbeans',
+            'macgdbp': 'macgdbp',
+            'PHPSTORM': 'PHPStorm',
+        };
+        this.handler = null;
+        this.checkIfEnabled();
+    }
+
+    checkIfEnabled() {
+        if (document.cookie.indexOf(this.cookieName) > -1) {
+            this.enabled = true;
+        }
+    }
+
+    show() {
+        if (null === this.handler) {
+            this.handler = document.createElement('div');
+            document.body.appendChild(this.handler);
+            this.handler.style = 'display: block; position: fixed; bottom: 0; left: 0; background: #000; color: #fff; font-size: 12px; text-align: left; padding: 2px; cursor: pointer;';
+            this.handler.classList.add('xdebug-helper');
+            this.handler.addEventListener('click', () => {
+                this.enabled = !this.enabled;
+                if (true === this.enabled) {
+                    this.setCookie(this.cookieName, 'PHPSTORM');
+                }
+                if (false === this.enabled) {
+                    this.unsetCookie(this.cookieName);
+                }
+                this.show();
+            });
+        }
+        if (true === this.enabled) {
+            this.handler.innerHTML = 'Xdebug is enabled.'
+            this.handler.style.color = '#32a309'
+        }
+        if (false === this.enabled) {
+            this.handler.innerHTML = 'Xdebug is disabled.'
+            this.handler.style.color = '#d50e0e'
+        }
+    }
+
+    setCookie(name, value) {
+        const date = new Date();
+        date.setTime(date.getTime() + (666 * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    unsetCookie(name) {
+        document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    new XdebugHelper()
+        .show()
+    ;
+});
